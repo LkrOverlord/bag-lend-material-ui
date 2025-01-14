e920e9828a50be6c1a914e5566efd2c1374279b7
LkrOverlord <menesestapia@gmail.com>
2025-01-13 23:27:50 -0300

    feat: complete navbar layout without functionality

--- components/shared/navbar/ProfileButton.tsx
+++ components/shared/navbar/ProfileButton.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import React, { useState } from 'react';
-import { IconButton, Menu, MenuItem } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Grid2, IconButton, Menu, MenuItem, Stack } from '@mui/material';
+import Image from 'next/image';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ProfileAvatar from '@/public/assets/profileAvatar.svg';
 
 const ProfileButton = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -24,14 +26,30 @@ const ProfileButton = () => {
         aria-haspopup="true"
         onClick={handleMenu}
         color="inherit"
+        sx={{
+          backgroundColor: 'white',
+          borderRadius: '24px',
+          padding: '4px',
+          '&:hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          },
+        }}
       >
-        <AccountCircle />
+        <Stack direction="row" alignItems="center">
+          <Image
+            src={ProfileAvatar}
+            alt="Profile"
+            width={24}
+            height={24}
+          />
+          <KeyboardArrowDownIcon sx={{ fontSize: 20, color:"black" }} />
+        </Stack>
       </IconButton>
       <Menu
         id="menu-appbar"
         anchorEl={anchorEl}
         anchorOrigin={{
-          vertical: 'top',
+          vertical: 'bottom',
           horizontal: 'right',
         }}
         keepMounted
@@ -41,11 +59,17 @@ const ProfileButton = () => {
         }}
         open={Boolean(anchorEl)}
         onClose={handleClose}
+        sx={{
+          marginTop: '8px',
+        }}
       >
         <MenuItem onClick={handleClose}>Perfil</MenuItem>
         <MenuItem onClick={handleClose}>Mi cuenta</MenuItem>
         <MenuItem onClick={handleClose}>Cerrar sesión</MenuItem>
       </Menu>
+      <Grid2>
+        
+      </Grid2>
     </div>
   );
 };
